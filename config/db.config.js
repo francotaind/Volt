bb18648683a6caaf026ef2f6baebfc05d01bfa4a
francotaind <francotaind00@gmail.com>
2024-11-03 22:41:52 +0300

    Addition of few add file configs

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,17 @@
+require('dotenv').config(); // Load environment variables
 const mysql = require('mysql2');
+const fs = require('fs');
 
-// Create the MySQL connection using your provided credentials
+// Create the MySQL connection using the environment variables
 const connection = mysql.createConnection({
-  host: 'mysql-2fb9d0c4-francotaind00-7296.g.aivencloud.com',
-  user: 'avnadmin',
-  password: 'AVNS_yL1i_Pji1Qcv-GNNpws',  // Use the password you provided
-  database: 'message_board',
-  port: 11395,  // Specify the port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: true,  // Enforce SSL connection
-    ca: require('fs').readFileSync(__dirname + '/ca-cert.pem')  // Path to the CA certificate
+    ca: fs.readFileSync(process.env.DB_SSL_CA)  // Path to the CA certificate
   }
 });
 
